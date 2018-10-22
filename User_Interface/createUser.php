b649273d3a506cf7f4dce5527fa43710031021ef
Emilia Paz <emilia.paz@ku.edu>
2018-10-21 21:48:25 -0500

    [ex 2,3] style forms

--- User_Interface/createUser.php
+++ User_Interface/createUser.php
@@ -2,35 +2,30 @@
     $mysqli = new mysqli("mysql.eecs.ku.edu", "e155p319", "eecs448", "e155p319");
 
     /* check connection */
-    if ($mysqli->connect_error) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_errno);
         exit();
     }
 
     $username = $_POST["username"];
     $query = "SELECT * FROM Users WHERE user_id = $username" ;
 
-    if ($result = $mysqli->query($query)) {
+    // User already exists
+    $result = mysqli_query($mysqli, "SELECT * FROM Users WHERE user_id='$username';");
 
-        /* fetch associative array */
-        if ($row = $result->fetch_assoc()) {
-            // tell the user its not valid, and stay there
-            echo "Username already exists" ;
-        }
-        else {
-            // create entry
-            $entry = "INSERT INTO Users(user_id) VALUES ('$username')";
-        }
+    if (mysqli_num_rows($result)>0){
+        echo "Username already exists" ;
+    }
 
+    // Create user
+    else{
+        $entry = "INSERT INTO Users (user_id) VALUES ('$username')";
         if ($mysqli->query($entry) === TRUE) {
             echo "New record created successfully";
         }
         else {
-            echo "Error: " ;
+            echo "Error" ;
         }
-
-        /* free result set */
-        $result->free();
     }
 
     /* close connection */
